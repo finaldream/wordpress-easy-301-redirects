d0b1f72a3e413e3b1f6a33b752e6041b99b9e10c
Carlos González Rico <cgonzalezr@gmail.com>
2019-11-24 18:33:03 +0100

    style of paginator

--- src/components/paginator.tsx
+++ src/components/paginator.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
-import Pagination from "react-js-pagination";
-import styled from 'styled-components';
+import Pagination from 'react-js-pagination';
 
 import { Dispatch } from '../lib/redirects-manager-state';
 
@@ -13,20 +12,7 @@ interface PaginatorComponentProps {
     dispatch: Dispatch;
 }
 
-interface PageSelectorProps { pageNumbers: number[]; current: number; dispatch: Dispatch; }
-
 interface PerPageSelectorProps { perPage: number; dispatch: Dispatch; }
-
-const PaginationWrapper = styled.div`
-    .pagination {
-        display: inline-flex;
-    }
-    li {
-        margin: 5px;
-        padding: 5px;
-        border: solid 1px blue;       
-    }
-`;
 
 const getPageNumbers = (viewLength: number, perPage: number) => {
     const pageNumbers = [];
@@ -37,46 +23,21 @@ const getPageNumbers = (viewLength: number, perPage: number) => {
     return pageNumbers;
 };
 
-const validateSelectedPage = (input: string, max: number) => {
-    const page = Number(input);
-    if (page < 1) { return 1; }
-    if (page > max) { return max; }
-    return page;
-};
-
-const PerPageSelector = ({perPage, dispatch} : PerPageSelectorProps) => {
+const PerPageSelector = ({perPage, dispatch}: PerPageSelectorProps) => {
     const options = [25, 50, 100];
     return (
         <div style={{display: 'inline-flex', marginLeft: '30px'}}>
         Show:&nbsp;   {
         options.map((option) => {
             const optionHTML = (option !== perPage) ?
-                <a style={{cursor: 'pointer'}} onClick={(e) => dispatch({type: 'set', value: {perPage: option} })}>{option}</a> :
+                <a style={{ cursor: 'pointer' }} onClick={
+                    () => dispatch({type: 'set', value: {perPage: option, currentPage: 1}
+                })}>{option}</a> :
                 <span style={{cursor: 'default'}} >{option}</span>;
             const separator = (Math.max(...options) !== option) ? '|' : null;
-            return <div key={option}>&nbsp;{optionHTML}&nbsp;{separator}</div>
+            return <div key={option}>&nbsp;{optionHTML}&nbsp;{separator}</div>;
         })
         }</div>
-    )
-}
-
-const PageSelector = ( { pageNumbers, current, dispatch }: PageSelectorProps) => {
-    const currentPage = current ? current : 1;
-    const lastPage = pageNumbers[pageNumbers.length - 1];
-    return (
-        <div className="alignright actions" style={{display: 'flex'}}>
-            <span  style={{ marginTop: '6px'}}>Page </span>
-            <input type="number"
-                style={{width: '35px', background: 'transparent', cursor: 'default', border: 'none'}}
-                min={pageNumbers[0]}
-                max={lastPage}
-                value={currentPage}
-                onChange={ (e) => dispatch({
-                    type: 'set-current-page',
-                    value: validateSelectedPage(e.target.value, lastPage)
-                })} />
-            <span  style={{marginLeft: '5px', marginTop: '6px'}}>of {lastPage}</span>
-        </div>
     );
 };
 
@@ -92,20 +53,19 @@ export const Paginator = ({
         <table className="widefat" id="paginator" style={{width: '100%'}}>
             <tr>
                 <th>
-                    <div className="alignleft actions" style={{display: 'flex'}}>
+                    <div className="alignleft actions" style={{display: 'flex', marginTop: '15px'}}>
                         Redirects: {redirectsLength} { filteredCount }
                         <PerPageSelector perPage={perPage} dispatch={dispatch} />
                     </div>
                     <div className="alignright actions" style={{display: 'flex'}}>
-                        <PaginationWrapper>
-                        <Pagination 
+                        <Pagination
                             activePage={currentPage}
                             itemsCountPerPage={perPage}
                             totalItemsCount={viewLength}
                             pageRangeDisplayed={5}
                             onChange={(pageNumber: number) => dispatch({type: 'set', value: {currentPage: pageNumber}})}
-                            />
-                        </PaginationWrapper>
+                            itemClass={'button'}
+                        />
                     </div>
                 </th>
             </tr>
